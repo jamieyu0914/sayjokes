1a83b09fe587341e5cb30d552b0f5c5dd4c5ac02
jamieyu0914 <jamieyu0914@gmail.com>
2023-07-09 12:56:49 +0800

    4st develop Commit 0709
    1. try to add "css/", "images/", "js/", "plugins/" of "statics"
    
    	modified:   sayjokes/__pycache__/settings.cpython-310.pyc
    	modified:   sayjokes/__pycache__/urls.cpython-310.pyc
    	modified:   sayjokes/__pycache__/views.cpython-310.pyc
    	modified:   sayjokes/settings.py
    	modified:   sayjokes/views.py
    	new file:   statics/css/chat.css
    	new file:   statics/js/chat.js
    	modified:   templates/index.html

--- sayjokes/sayjokes/settings.py
+++ sayjokes/sayjokes/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/' #路徑的別名
+STATICFILES_DIRS=[
+    Path(BASE_DIR,"statics")
+]
